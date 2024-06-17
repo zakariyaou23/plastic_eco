c7023b88c6d59bed930f2d99ab6c3d4cae0104b9
zakariyaou23 <zakariyaousani6@gmail.com>
2024-06-17 20:33:04 +0100

    IMS ccarousel updates

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -1,9 +1,11 @@
+import GalleryList from "@/components/guest/carousel";
 import Hero from "@/components/guest/hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <GalleryList />
     </>
   );
 }
